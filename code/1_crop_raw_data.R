950af7496b470a98507a44ecd932c43c1f894148
Jess Kunke <jessicakunke@gmail.com>
2025-11-19 23:00:51 -0800

    Reorganize

--- code/1_crop_raw_data.R
+++ code/1_crop_raw_data.R
@@ -11,20 +11,20 @@ library(tidyr)
 library(ggplot2)
 
 # base folder
-base_loc <- "D:\\Hoh tribe Bray Water Quality\\Water quality monitoring program\\Continuous temperature\\Data\\2022\\2022 summer_NEW\\2. Cropped files\\"
+base_loc <- "2023_fall_data/"
 
 # input folder; directory containing your raw data
-rawdata_loc <- paste0(base_loc, "1. Raw csv\\")
+rawdata_loc <- paste0(base_loc, "1_raw_csv/")
 
 # output folder; directory where your cropped data will be (or currently is) stored
-cropped_loc <- paste0(base_loc, "2. Cropped csv\\")
+cropped_loc <- paste0(base_loc, "2_cropped_csv/")
 
 # also an output folder; directory where plots of cropped and raw data will be stored
-croppedplots_loc <- paste0(base_loc, "3. Cropped plots\\")
+croppedplots_loc <- paste0(base_loc, "3_cropped_plots/")
 
 # name of your LDRTimes file that has the lookup table with deployment/retrieval dates
-# assumes ldrtimes_fn is in rawdata_loc folder
-ldrtimes_fn <- "LDRTimes_summer22.xlsx"
+# note: the line below assumes ldrtimes_fn is in rawdata_loc folder
+ldrtimes_fn <- "ldrtimes.xlsx"
 
 # -------------------------------------------
 # editing mostly takes place above this line
@@ -52,14 +52,14 @@ for(this.file in csv_files){
   i = i + 1
   #this.file = csv_files[1] # uncomment to troubleshoot within loop
   cat(paste0("Reading file ", i, " of ", length(csv_files), ": ", this.file), fill = TRUE)
-  
+
   # extract metadata from the filename
   filename.parts = stringr::str_split_1(this.file, '[_.]')
   csv.site = filename.parts[1]
   csv.media = filename.parts[2]
   csv.season = filename.parts[3]
   csv.year = filename.parts[4]
-  
+
   # convert the character-format datetime to an R POSIXct object
   # ymd_hm is the format the character string is in initially; it tells R
   # how to read and interpret the character string
@@ -89,7 +89,7 @@ for(this.file in csv_files){
         dplyr::mutate(datetime = lubridate::mdy_hm(datetime)) #for datetime in hh:mm
     }
   )
-  
+
   # crop the data
   deploy.retrieval = ldrtimes %>%
     # select the row(s) of ldrtimes that match this datafile
@@ -99,29 +99,29 @@ for(this.file in csv_files){
                   deploy_year == csv.year, media == csv.media) %>%
     # keep just the deploy_time and retrieval_time variables/columns
     dplyr::select(deploy_time, retrieval_time)
-  
+
   if(nrow(deploy.retrieval) == 0){
     stop("no rows of ldrtimes matched this csv file.")
   }
   if(nrow(deploy.retrieval) > 1){
     stop("multiple rows of ldrtimes matched this csv file.")
   }
-  
+
   deploy = deploy.retrieval$deploy_time
   retrieval = deploy.retrieval$retrieval_time
-  
+
   if(retrieval > deploy) {
     cropped.data = dplyr::filter(this.data,
                                  datetime > deploy,
                                  datetime < retrieval)
-    
+
   } # if(retrieval > deploy)
-  
+
   # write cropped csv files to cropped folder
   readr::write_csv(cropped.data,
                    file=paste0(cropped_loc,
                                stringr::str_split_i(this.file, "[.]", 1), "_cropped.csv"))
-  
+
   #Create a dataframe of the raw and cropped data
   cropvraw <- dplyr::left_join(this.data, cropped.data, by=c("row.num", "datetime")) %>%
     dplyr::rename(raw.temp = temperature.x,
@@ -129,7 +129,7 @@ for(this.file in csv_files){
     #create new column of data type (raw or cropped for plotting in ggplot)
     tidyr::pivot_longer(cols = raw.temp:cropped.temp,
                         names_to="type", values_to="temp")
-  
+
   cropvraw.plot <- ggplot2::ggplot(cropvraw,
                                    ggplot2::aes(x = datetime,
                                                 y = temp,
@@ -139,12 +139,12 @@ for(this.file in csv_files){
     ggplot2::labs(title = paste0(" Raw versus Cropped data"),
                   x = "Date", y = "Temperature (C)")+
     ggplot2::theme(axis.text = ggplot2::element_text(colour = "black", size = (12)))
-  
-  ggplot2::ggsave(paste0(croppedplots_loc, csv.site, "_", 
+
+  ggplot2::ggsave(paste0(croppedplots_loc, csv.site, "_",
                          csv.media, "_rawvscroppeddata.png"),
                   cropvraw.plot,
                   width = 11, height = 8.5, units = "in")
-  
+
 }; cat("Done.", fill = TRUE)
 
 
